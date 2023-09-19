44902d7e521805765a638797b5b6f83504bc1f01
yanzhai nie <yanzhai.nie@outlook.com>
2023-09-19 16:03:34 +0800

    read configration from json using config crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use std::net::TcpListener;
+use zero2prod::configuration::get_configurations;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
+    let settings = get_configurations().expect("Failed to read configuration.");
+
+    let listener = TcpListener::bind(&format!("127.0.0.1:{}", settings.application_port))?;
     zero2prod::startup::run(listener)?.await
 }
