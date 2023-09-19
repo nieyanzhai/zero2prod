03b08b2ea56eef1320407bd1d76de02f4f62cf34
yanzhai nie <yanzhai.nie@outlook.com>
2023-09-19 16:24:42 +0800

    update test to check db subscriptions

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,22 @@
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configurations;
+
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
-    let addr = spawn_app();
+    let settings = get_configurations().expect("Failed to read configuration.");
+    let connection_string = settings.database.connection_string();
+    let mut database = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let app_addr = spawn_app();
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     // Act
     let response = client
-        .post(&format!("{}/subscriptions", addr))
+        .post(&format!("{}/subscriptions", app_addr))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -16,12 +25,19 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut database)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!("le guin", saved.name);
+    assert_eq!("ursula_le_guin@gmail.com", saved.email);
 }
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
-    let addr = spawn_app();
+    let app_addr = spawn_app();
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
@@ -32,7 +48,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     // Act
     for (invalid_body, error_message) in test_cases {
         let response = client
-            .post(&format!("{}/subscriptions", addr))
+            .post(&format!("{}/subscriptions", app_addr))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
@@ -47,12 +63,12 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    let addr = spawn_app();
+    let app_addr = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get(&format!("{}/health_check", addr))
+        .get(&format!("{}/health_check", app_addr))
         .send()
         .await
         .expect("Failed to execute request");
