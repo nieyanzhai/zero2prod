ce7caeb124de3c1d57867eaa5e334ab44f9963e6
yanzhai nie <yanzhai.nie@outlook.com>
2023-09-20 09:58:39 +0800

    Persisting a new subscriber
    dependency injection:
      - web::data<PgPool>
    
    handlers:
      - subscribe(): persisting a new subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use actix_web::web;
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configurations;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let settings = get_configurations().expect("Failed to read configuration.");
+    let pg_pool = PgPool::connect(&settings.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    let db_pool = web::Data::new(pg_pool);
 
     let listener = TcpListener::bind(&format!("127.0.0.1:{}", settings.application_port))?;
-    zero2prod::startup::run(listener)?.await
+    zero2prod::startup::run(listener, db_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,8 @@
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
 use serde::Deserialize;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 pub struct FormData {
@@ -10,6 +13,24 @@ pub struct FormData {
 /// extract form data using serde
 /// this handler gets called only if the content type is *x-www-form-urlencoded*
 /// and the content of the request could be deserialized to a `FormData` struct
-pub async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok()
+pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, name, email, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.name,
+        form.email,
+        Utc::now()
+    )
+    .execute(db_pool.as_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
