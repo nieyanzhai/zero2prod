946ef7ae23589dd5be15ad2b5fa7266644ce6c06
yanzhai nie <yanzhai.nie@outlook.com>
2023-09-21 09:00:28 +0800

    structured log
    how:
      - tracing: set global subscriber
      - tracing-subscriber: set filter, formatter
      - tracing-bunyan-formatter: provide formatter layer
      - other things: tracing span and instrument, check out subsciptions.rs for details example

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -13,13 +14,22 @@ pub struct FormData {
 /// extract form data using serde
 /// this handler gets called only if the content type is *x-www-form-urlencoded*
 /// and the content of the request could be deserialized to a `FormData` struct
-pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    connection_pool: web::Data<PgPool>,
+) -> HttpResponse {
     let uuid = Uuid::new_v4();
-    log::info!(
-        "request id {uuid} - Adding {} {} as a new subscriber",
-        form.email,
-        form.name
-    );
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %uuid,
+        subscriber_email=%form.email,
+        subscriber_name=%form.name);
+    let _request_span_guard = request_span.enter();
+
+    // We do not call `.enter` on query_span!
+    // `.instrument` takes care of it at the right moments
+    // in the query future lifetime
+    let query_span = tracing::info_span!("Saving new subscriber to database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, name, email, subscribed_at)
@@ -30,15 +40,16 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
         form.email,
         Utc::now()
     )
-    .execute(db_pool.as_ref())
+    .execute(connection_pool.as_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            log::info!("request id {uuid} - New subscriber details have been saved",);
+            tracing::info!("Subscriber added to database.");
             return HttpResponse::Ok().finish();
         }
         Err(e) => {
-            log::error!("request id {uuid} - Failed to execute query: {:#?}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
