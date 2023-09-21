f415c794627245d12b6dd15b6671d057c5a2f379
yanzhai nie <yanzhai.nie@outlook.com>
2023-09-21 09:23:44 +0800

    cleaning up initialization
    how:
      - create fn get_subscriber(name, env_filter)
      - create fn init_subscriber(subscriber)
      - refactor main.rs to use the 2 newly created fn
      - create new mod telemetry, and move the 2 newly created fn to it, and update the use in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use zero2prod::configuration::get_configurations;
+use zero2prod::{configuration, telemetry};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::from("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into());
+    telemetry::init_subscriber(subscriber);
 
-    let configurations = get_configurations().expect("Failed to read configuration.");
+    let configurations =
+        configuration::get_configurations().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configurations.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
